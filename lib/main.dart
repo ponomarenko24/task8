26e4d7318b6f9c79fc2d05ba053f129955829fba
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-04-10 16:46:29 +0300

    Added ListView.bulder

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,27 @@ void main() {
   runApp(const MainApp());
 }
 
+List<int> items = [1,2,3,4,5,6,7,8,9,10];
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 150,
+                margin: EdgeInsets.all(20),
+                color: Colors.amber,
+                child: Center(child: Text("data ${items[index]}")),
+              );
+              },),
         ),
       ),
     );
