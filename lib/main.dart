6ad6911af78f2c0d7fbfd561b3fc1d405cff8264
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-04-10 23:20:22 +0300

    Added Dialog window for Floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODO List',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MainApp(),
+    );
+  }
 }
 
 List<int> items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -36,14 +48,39 @@ class _MainAppState extends State<MainApp> {
           },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              items.insert(0, 100);
-            });
-          },
+          onPressed: () => dialogWindow(context),
           child: Icon(Icons.add),
         ),
       ),
+    );
+  }
+
+  Future<String?> dialogWindow(BuildContext context) {
+    return showDialog<String>(
+      context: context,
+      builder:
+          (BuildContext context) => Dialog(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 30),
+                  TextField(
+
+                  ),
+                  const SizedBox(height: 30),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Close'),
+                  ),
+                ],
+              ),
+            ),
+          ),
     );
   }
 }
