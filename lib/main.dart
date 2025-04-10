552f174c30c0ed7304adcdd93ebe0d4606fa160b
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-04-10 20:02:37 +0300

    Added Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,21 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: items.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 150,
-                margin: EdgeInsets.all(20),
-                color: Colors.amber,
-                child: Center(child: Text("data ${items[index]}")),
-              );
-            },
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.grey[300],
+          title: Text("TODO List"),
+        ),
+        body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 150,
+              margin: EdgeInsets.all(20),
+              color: Colors.amber,
+              child: Center(child: Text("data ${items[index]}")),
+            );
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
